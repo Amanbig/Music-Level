2162a2f4cd2a8e5fd018ca046a2e2337edc0fb76
Amanpreet Singh <amanpreetsinghjhiwant@outlook.com>
2024-12-15 20:41:14 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:music_level/screens/auth/login_screen.dart';
+import 'package:music_level/screens/auth/signup_screen.dart';
+import 'package:music_level/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      initialRoute: '/', // Set initial route to Login page
+      routes: {
+        '/' : (context) => const MainScreen(),
+        '/login': (context) => const LoginPage(),
+        '/signup': (context) => const SignUpPage(),
+      },
+    );
+  }
+}
